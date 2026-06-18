b59211d22d5d89c6347585827050decdaeff77be
Serge Goobeen <dr.aiworker@gmail.com>
2026-06-18 11:57:23 +0500

    Initial Vite React project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="app">
+      <h1>Usmanova Landing Test</h1>
+      <p>Mini landing: offer -&gt; trust -&gt; programs -&gt; lead form</p>     
+    </main>
+  )
+}
+
+export default App
